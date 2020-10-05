4dcabd1bf15ae1e0d8d269cf5c79dfc7b21a2068
Ruslan <67142945+ruslanchampion@users.noreply.github.com>
2020-10-05 10:07:34 +0800

    feat: add desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let regexp = /(\()(\))|(\[)(\])|(\{)(\})|(\|)(\|)|(1)(2)|(3)(4)|(5)(6)|(7)(7)|(8)(8)/;
+  while (str.search(regexp)!=-1) {
+    str = str.replace(regexp, '');
+  }
+  return (str=='') ? true : false;
 }
